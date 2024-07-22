ec981d7470e46a124c81be306c3cfc953488f4d7
Krzysztof <krzysztof@dawidrza.com>
2024-07-22 22:30:36 +0200

    tłumaczenia

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import translationEN from "./locales/en/translation.json";
+import translationPL from "./locales/pl/translation.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: translationEN,
+    },
+    pl: {
+      translation: translationPL,
+    },
+  },
+  lng: navigator.language.split("-")[0],
+  fallback: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
