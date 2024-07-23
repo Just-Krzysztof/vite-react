69f378757f67538067f7a0a3699625d700db65b4
Krzysztof <krzysztof@dawidrza.com>
2024-07-23 17:44:06 +0200

    animation translation button fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import "./App.css";
 import { changeLanguage } from "i18next";
 import { useEffect } from "react";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   const { t, i18n } = useTranslation();
@@ -21,16 +22,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-header-smear">
+        <div>
           <h1>Curriculum Vitae</h1>
         </div>
 
         <div className="language-swithcer">
           <button
             onClick={() =>
-              handleLanguageChange(
-                i18n.language === "en"? "pl" : "en"
-              )
+              handleLanguageChange(i18n.language === "en" ? "pl" : "en")
             }
           >
             {i18n.language === "en" ? "Polish" : "English"}
@@ -39,58 +38,17 @@ function App() {
       </header>
 
       <div className="content">
-        <div className="sidebar">
-          <h2>Krzysztof Just</h2>
-          <p>
-            <span>{t("email")}:</span>
-            <a href="mailto:krzysztofjustjust@gmail.com">
-              krzysztofjustjust@gmail.com
-            </a>
-          </p>
-          <p>
-            <span>{t("phone")}:</span> +48 511 008 612
-          </p>
-          <p>
-            <span>{t("location")}:</span> Gdynia, {t("")}
-          </p>
-          <h2>{t("languages")}</h2>
-          <p>
-            {t("english")}:<span> {t("intermediate")}</span>
-          </p>
-          <p>
-            {t("polish")}: <span>{t("native")}</span>
-          </p>
-          <h2>{t("skills")}</h2>
-          <p>
-            <span>{t("programming languages")}:</span> JavaScript, TypeScript,
-            Python
-          </p>
-          <p>
-            <span>{t("frameworks")}:</span> Vue.js, React.js, Django
-          </p>
-          <p>
-            <span>{t("front-end technologies")}:</span> CSS, SCSS
-          </p>
-          <p>
-            <span>API:</span> RESTful API
-          </p>
-          <p>
-            <span>{t("projects")}:</span> {t("projects desc")}
-          </p>
-          <p>
-            <span>{t("soft skills")}:</span> {t("soft skills desc")}
-          </p>
-          <p>
-            <span>{t("other")}:</span> {t("other desc")}
-          </p>
-        </div>
+        <Sidebar></Sidebar>
+        
         <div className="main-content">
           <h2>{t("professional summary")}</h2>
           <p>
             <i>{t("i am")}</i>
           </p>
           <h2>{t("professional experience")}</h2>
-          <h3>{t("frontend developer")}</h3>
+          <h3>
+            <span>{t("frontend developer")}</span>
+          </h3>
           <p>Rząska Poland, Gdynia, {t("poland")}</p>
           <p>
             {t("november")} 2022 – {t("present")}
@@ -102,7 +60,9 @@ function App() {
             <li>{t("performance")}</li>
             <li>{t("optymalized")}</li>
           </ul>
-          <h3>{t("kids")}</h3>
+          <h3>
+            <span>{t("kids")}</span>
+          </h3>
           <p>Academy Spice Gears, Łomża, {t("poland")}</p>
           <p>
             {t("march")} 2021 – {t("november")} 2022
@@ -113,17 +73,21 @@ function App() {
             <li>{t("communicated")}</li>
             <li>{t("environment")}</li>
           </ul>
-          <h3>{t("intern")}</h3>
+          <h3>
+            <span>{t("intern")}</span>
+          </h3>
           <p>Transition Technologies MS S.A., Koszalin, {t("poland")}</p>
           <p>
-            {t("july")}2022 – {t("eptember")}2022
+            {t("july")} 2022 – {t("september")} 2022
           </p>
           <ul>
             <li>{t("gitlab")}</li>
             <li>{t("angular")}</li>
             <li>{t("implement")}</li>
           </ul>
-          <h3>{t("graphic intern")}</h3>
+          <h3>
+            <span>{t("graphic intern")}</span>
+          </h3>
           <p>Logo s.c., Łomża, {t("poland")}</p>
           <p>
             {t("march")} 2022 – {t("july")} 2022
@@ -143,25 +107,35 @@ function App() {
           <h2>{t("courses and certifications")}</h2>
           <p>JavaScript – Jedyny kurs jakiego potrzebujesz</p>
           <p>
-            {t("organizer")}: Mateusz Maj, {t("january")} 2022
+            {t("organizer")}: Mateusz Maj, <span>{t("january")} 2022</span>
           </p>
-          <p>{t("e14")}</p>
-          <p>{t("january")} 2019</p>
-          <p>{t("e13")}</p>
-          <p>{t("may")} 2018</p>
-          <p>{t("e12")}</p>
-          <p> {t("may")}2017</p>
-          <h2>{t("activities")}</h2>
-          <p>{t("presidium")}</p>
           <p>
-            Państwowa Wyższa Szkoła Informatyki i Przedsiębiorczości w Łomży,{" "}
-            {t("october")} 2020 – {t("october")} 2021
+            {t("e14")}, <span>{t("january")} 2019</span>
+          </p>
+          <p>
+            {t("e13")}, <span>{t("may")} 2018</span>
+          </p>
+          <p>
+            {t("e12")}, <span>{t("may")} 2017</span>
           </p>
           <h2>{t("interests")}</h2>
-          <p>{t("interests p1")}</p>
-          <p>{t("interests p2")}</p>
-          <p>{t("interests p3")}</p>
-          <p>{t("interests p4")}</p>
+          <p>
+            <span>{t("interests p1")}: </span>
+            {t("interests p1-1")}
+          </p>
+          <p>
+            <span>{t("interests p2")}: </span>
+            {t("interests p2-2")}
+          </p>
+          <p>
+            <span>{t("interests p3")}: </span>
+            {t("interests p3-3")}
+          </p>
+          <p>
+            <span>{t("interests p4")}: </span>
+
+            {t("interests p4-4")}
+          </p>
         </div>
       </div>
     </div>
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,65 @@
+import "../App.css";
+import { useTranslation } from "react-i18next";
+
+function Sidebar() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="sidebar">
+      <h2>Krzysztof Just</h2>
+      <p>
+        <span>{t("email")}: </span>
+        <a href="mailto:krzysztofjustjust@gmail.com">
+          krzysztofjustjust@gmail.com
+        </a>
+      </p>
+      <p>
+        <span>{t("phone")}: </span> +48 511 008 612
+      </p>
+      <p>
+        <span>{t("location")}: </span>
+        <a
+          href="https://www.google.com/maps?q=Gdynia,Poland"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gdynia, {t("poland")}
+        </a>
+      </p>
+      <h2>{t("languages")}</h2>
+      <p>
+        {t("english")}: <span>{t("intermediate")}</span>
+      </p>
+      <p>
+        {t("polish")}: <span>{t("native")}</span>
+      </p>
+      <h2>{t("skills")}</h2>
+      <p>
+        <span>{t("programming languages")}: </span> JavaScript, TypeScript,
+        Python
+      </p>
+      <p>
+        <span>{t("frameworks")}: </span> Vue.js, React.js, Django
+      </p>
+      <p>
+        <span>{t("front end")}: </span> CSS, SCSS
+      </p>
+      <p>
+        <span>API: </span> RESTful API
+      </p>
+      <p>
+        <span>{t("projects")}: </span>
+        {t("projects desc")}
+      </p>
+      <p>
+        <span>{t("soft skills")}: </span>
+        {t("soft skills desc")}
+      </p>
+      <p>
+        <span>{t("other")}: </span>
+        {t("other desc")}
+      </p>
+    </div>
+  );
+}
+export default Sidebar;
