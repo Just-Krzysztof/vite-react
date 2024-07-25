0926bb73259c7329be679bdc5c7b4872bdbadb6a
Krzysztof <krzysztof@dawidrza.com>
2024-07-25 19:53:56 +0200

    Testy

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,6 +1,9 @@
 function Portfolio() {
-    return (
-        <h1>test</h1>
-    )
+  return (
+    <>
+      <h1>test</h1>
+      <h1>Big name here</h1>
+    </>
+  );
 }
-export default Portfolio
+export default Portfolio;
